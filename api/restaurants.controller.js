156fc286f3c14d6393dc628174efc2bf16cb1cee
Robert Tiempo <jroberttiempo58@gmail.com>
2022-02-24 21:59:58 +0800

    Add restaurant search by id and cuisine

--- api/restaurants.controller.js
+++ api/restaurants.controller.js
@@ -28,8 +28,32 @@ export default class RestaurantsController {
       page: page,
       filters: filters,
       entries_per_page: restaurantsPerPage,
-      total_resutls: totalNumRestaurants,
+      total_results: totalNumRestaurants,
     };
     res.json(response);
+  }
+  static async apiGetRestaurantById(req, res, next) {
+    try {
+      let id = req.params.id || {};
+      let restaurant = await RestaurantsDAO.getRestaurantByID(id);
+      if (!restaurant) {
+        res.status(404).json({ error: 'Not found' });
+        return;
+      }
+      res.json(restaurant);
+    } catch (e) {
+      console.log(`api, ${e}`);
+      res.status(500).json({ error: e });
+    }
+  }
+
+  static async apiGetRestaurantCuisines(req, res, next) {
+    try {
+      let cuisines = await RestaurantsDAO.getCuisines();
+      res.json(cuisines);
+    } catch (e) {
+      console.log(`api, ${e}`);
+      res.status(500).json({ error: e });
+    }
   }
 }
